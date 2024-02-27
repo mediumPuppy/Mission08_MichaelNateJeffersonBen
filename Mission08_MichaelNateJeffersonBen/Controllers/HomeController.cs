77f587e1d2203bf0b73d8837a4463e0599e3706f
Ben Macmichael <benjaminmac21@gmail.com>
2024-02-26 23:11:38 -0700

    Created Get controller for Update page Nate made

--- Mission08_MichaelNateJeffersonBen/Controllers/HomeController.cs
+++ Mission08_MichaelNateJeffersonBen/Controllers/HomeController.cs
@@ -10,5 +10,30 @@ namespace Mission08_MichaelNateJeffersonBen.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult Update(Model response) 
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        //Add record to the database and save it
+        //        _context.Model.Add(response);
+        //        _context.SaveChanges();
+
+        //        return View("Confirmation");
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Blah = _context.Blah
+        //        .OrderBy(x => x.Category)
+        //        .ToList();
+        //        return View(response);
+        //    }
+        //}
     }
 }
